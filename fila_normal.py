fdaafd14c27fdc3ec1451724d6f28c621470a76d
Andre Pinheiro <apinheiro99@gmail.com>
2022-02-16 21:09:14 -0500

    Usando Flake8

--- fila_normal.py
+++ fila_normal.py
@@ -1,24 +1,24 @@
 class filanormal:
-    codigo:int = 0
+    codigo: int = 0
     fila = []
     clientesatendidos = []
-    senhaatual:str = ""
+    senhaatual: str = ""
 
-    def gerasenhaatual(self)-> None:
+    def gerasenhaatual(self) -> None:
         self.senhaatual = f"NM{self.codigo}"
 
-    def resetafila(self)-> None:
+    def resetafila(self) -> None:
         if self.codigo >= 100:
             self.codigo = 0
         else:
             self.codigo += 1
 
-    def atualizafila(self)-> None:
+    def atualizafila(self) -> None:
         self.resetafila()
         self.gerasenhaatual()
         self.fila.append(self.senhaatual)
 
-    def chamacliente(self, caixa:int)-> str:
+    def chamacliente(self, caixa: int) -> str:
         clienteatual: str = self.fila.pop(0)
         self.clientesatendidos.append(clienteatual)
         return f"Cliente atual: {clienteatual} dirija-se ao caixa {caixa}"
--- fila_prioritaria.py
+++ fila_prioritaria.py
@@ -4,26 +4,26 @@ class FilaPrioritaria():
     clientes_atendidos = []
     senha_atual: str = ""
 
-    def gera_senha_atual(self)-> None:
-       self.senha_atual = f"PR{self.codigo}"
+    def gera_senha_atual(self) -> None:
+        self.senha_atual = f"PR{self.codigo}"
 
-    def reseta_fila(self)-> None:
+    def reseta_fila(self) -> None:
         if self.codigo >= 100:
             self.codigo = 0
         else:
             self.codigo += 1
 
-    def atualiza_fila(self)-> None:
+    def atualiza_fila(self) -> None:
         self.reseta_fila()
         self.gera_senha_atual()
         self.fila.append(self.senha_atual)
 
-    def chama_cliente(self, caixa:int)-> str:
+    def chama_cliente(self, caixa: int) -> str:
         cliente_atual = self.fila.pop(0)
         self.clientes_atendidos.append(cliente_atual)
         return f"Cliente atual: {cliente_atual} dirija-se ao caixa {caixa}"
 
-    def estatistica(self, dia:str, agencia:int, flag:str)-> dict:
+    def estatistica(self, dia: str, agencia: int, flag: str) -> dict:
         if flag != "detail":
             estatistica = {f"{agencia} - {dia}": len(self.clientes_atendidos)}
         else:
@@ -31,7 +31,7 @@ class FilaPrioritaria():
             estatistica["dia"] = dia
             estatistica["agencia"] = agencia
             estatistica["Clentes atendidos"] = self.clientes_atendidos
-            estatistica["quantidade de clientes atendidos"] = len(self.clientes_atendidos)
-            
+            estatistica["quantidade de clientes atendidos"] = \
+                len(self.clientes_atendidos)
+
         return estatistica
-     
