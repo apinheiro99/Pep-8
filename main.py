75757720bb70513273224fdf20d9851548e58def
Andre Pinheiro <apinheiro99@gmail.com>
2022-02-20 22:41:37 -0500

    Construindo uma dependencia

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from constantes import TIPO_FILA_NORMAL
+from constantes import TIPO_FILA_NORMAL, TIPO_FILA_PRIORITARIA
+from estatistica_detalhada import EstatisticaDetalhada
+from estatistica_resumida import EstatisticaResumida
 from fila_normal import FilaNormal
 from fila_prioritaria import FilaPrioritaria
 from fabrica_fila import FabricaFila
@@ -20,7 +22,8 @@ fila_teste_2.atualiza_fila()
 fila_teste_2.atualiza_fila()
 print(fila_teste_2.chama_cliente(10))
 print(fila_teste_2.chama_cliente(7))
-print(fila_teste_2.estatistica("10/01/1980", 198, "detail"))
+print(fila_teste_2.estatistica("10/01/1980", 198, EstatisticaDetalhada))
+print(fila_teste_2.estatistica("10/01/1980", 198, EstatisticaResumida))
 
 print()
 teste_fabrica = FabricaFila.pega_fila(TIPO_FILA_NORMAL)
@@ -29,3 +32,13 @@ teste_fabrica.atualiza_fila()
 teste_fabrica.atualiza_fila()
 print(teste_fabrica.chama_cliente(5))
 print(teste_fabrica.chama_cliente(2))
+
+print()
+teste_fabrica = FabricaFila.pega_fila(TIPO_FILA_PRIORITARIA)
+teste_fabrica.atualiza_fila()
+teste_fabrica.atualiza_fila()
+teste_fabrica.atualiza_fila()
+print(teste_fabrica.chama_cliente(5))
+print(teste_fabrica.chama_cliente(2))
+print(fila_teste_2.estatistica("10/01/2080", 198, EstatisticaDetalhada))
+print(fila_teste_2.estatistica("10/01/2080", 198, EstatisticaResumida))
