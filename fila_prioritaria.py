4436c2c13c8903f0f2c600157431c9bd16761eb3
Andre Pinheiro <apinheiro99@gmail.com>
2022-02-20 22:19:40 -0500

    Reduzindo quantidade de ifs

--- fila_prioritaria.py
+++ fila_prioritaria.py
@@ -1,4 +1,4 @@
-from typing import Dict, List, Union
+# from typing import Dict, List, Union
 from fila_base import FilaBase
 from constantes import CODIGO_PRIORITARIO
 
@@ -13,16 +13,21 @@ class FilaPrioritaria(FilaBase):
         self.clientes_atendidos.append(cliente_atual)
         return f"Cliente atual: {cliente_atual} dirija-se ao caixa {caixa}"
 
-    def estatistica(self, dia: str, agencia: int, flag: str) -> dict:
-        estatistica: Dict[str, Union[List[str], str, int]] = {}
+    def estatistica(self, dia: str, agencia: int, retorna_estatistica) -> dict:
+        estatistica = retorna_estatistica(dia, agencia)
 
-        if flag != "detail":
-            estatistica[f"{agencia} - {dia}"] = len(self.clientes_atendidos)
-        else:
-            estatistica["dia"] = dia
-            estatistica["agencia"] = agencia
-            estatistica["Clentes atendidos"] = self.clientes_atendidos
-            estatistica["quantidade de clientes atendidos"] = \
-                len(self.clientes_atendidos)
+        return estatistica.roda_estatistica(self.clientes_atendidos)
 
-        return estatistica
+    # def estatistica(self, dia: str, agencia: int, flag: str) -> dict:
+    #     estatistica: Dict[str, Union[List[str], str, int]] = {}
+
+    #     if flag != "detail":
+    #         estatistica[f"{agencia} - {dia}"] = len(self.clientes_atendidos)
+    #     else:
+    #         estatistica["dia"] = dia
+    #         estatistica["agencia"] = agencia
+    #         estatistica["Clentes atendidos"] = self.clientes_atendidos
+    #         estatistica["quantidade de clientes atendidos"] = \
+    #             len(self.clientes_atendidos)
+
+    #     return estatistica
